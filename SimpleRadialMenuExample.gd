d7751344008dca17ad3cb4e4d82a9f208d0fb2f4
unknown <dwlee0802@gmail.com>
2024-07-06 14:18:06 +0900

    Fixed reference bug with test scene.

--- SimpleRadialMenuExample.gd
+++ SimpleRadialMenuExample.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _on_radial_menu_pressed(index):
 	print("Option " + str(index))
-	$Label.text = "Pressed option: " + str(index)
+	$OutputLabel.text = "Pressed option: " + str(index)
